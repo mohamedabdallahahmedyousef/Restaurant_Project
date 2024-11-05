90feed5ecd75f0eb833c1ab9aaa49f5ff7e2dfbf
Mohamed Abdallah <117682658+mohamedabdallahahmedyousef@users.noreply.github.com>
2024-11-05 08:58:01 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+
+// mg=document.querySelector('img')
+// source=mg.src;
+// mg.addEventListener('click', function(){
+// document.body.classList.toggle('dark');
+// document.querySelector('search').classList.toggle('dark');
+// mg.src="images/light.jpg"
+// if(!document.querySelector('search').classList.contains('dark)'))
+// {
+// mg.src="images/light.jpg"
+// }
+// else
+// {
+//     mg.src="images/moon.jpg";
+  
+// }
+
+// })
+
+// mg.addEventListener('click', function()){
+    function mode()
+    {
+        document.body.classList.toggle('dark');
+    }
